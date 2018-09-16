ad7ac0c6d472778ca784e28c45fd3ff1f10cb9ae
joao barbirato <joaobarbirato@gmail.com>
2018-09-16 17:44:00 -0300

    Method treatment added

--- server.py
+++ server.py
@@ -1,10 +1,40 @@
-#!/bin/python
-import socket
+#!/usr/bin/python
+import socket, select
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 # Menores que 1024 são restritos ao SO
-s.bind(('', 1025))
+s.bind(('', 1026))
 s.listen(5)
+c_inputs = []
+requests = {}
 while True:
-    client, addr = s.accept()
-    client.send(b'HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n <h1>Hello!</h1>')
-    client.close()
+    rlist, wlist, xlist = select.select(c_inputs + [s],[],[])
+    print(rlist)
+    for client in rlist:
+        if client ==s:
+            client, addr = s.accept()
+            
+            client.setblocking(0)
+            c_inputs.append(client)
+            requests[client] = b''
+        else:
+            requests[client] += client.recv(1500)
+            request = requests[client]
+            if b'\r\n\r\n' in request or b'\n\n' in request:
+                method, path, lixo = request.split(b' ', 2)
+                
+                if method == b'GET':
+                    print(path)
+                    if path == b'/':
+                        response_content = open('index.html','rb').read()
+                    else:
+                        response_content = open('generic_error.html','rb').read()
+                else:
+                    response_content = open('generic_error.html','rb').read()
+                
+                response = b'HTTP/1.0 200 OK\r\nContent-Length: %d\r\n\r\n' % len(response_content)
+                response += response_content
+                client.send(response)
+                client.close()
+                del requests[client]
+                c_inputs.remove(client)
