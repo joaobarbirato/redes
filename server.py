0c7414fa12786ff890e731e7bc36476275fae8a8
Spooks <g4briel.4lves@gmail.com>
2018-09-16 18:36:10 -0300

    Adicionando mensagens de erro dinâmicas

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
-import socket, select
+
+import select
+import socket
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 # Menores que 1024 são restritos ao SO
@@ -7,13 +10,14 @@ s.bind(('', 1026))
 s.listen(5)
 c_inputs = []
 requests = {}
+
 while True:
-    rlist, wlist, xlist = select.select(c_inputs + [s],[],[])
+    rlist, wlist, xlist = select.select(c_inputs + [s], [], [])
     print(rlist)
     for client in rlist:
-        if client ==s:
+        if client == s:
             client, addr = s.accept()
-            
+
             client.setblocking(0)
             c_inputs.append(client)
             requests[client] = b''
@@ -22,20 +26,24 @@ while True:
             request = requests[client]
             if b'\r\n\r\n' in request or b'\n\n' in request:
                 method, path, lixo = request.split(b' ', 2)
-                
+
                 if method == b'GET':
                     print(path)
                     if path == b'/':
-                        response_content = open('index.html','rb').read()
+                        response_content = open('index.html', 'rb').read()
+                        status = "200 OK"
                     else:
                         response_content = open('generic_error.html','rb').read()
+                        status = "404 Not Found"
                 elif method == b'POST':
                     print(method, path,"\n", lixo)
                     response_content = b"Arquivo recebido com sucesso xD"
+                    status = "200 OK"
                 else:
-                    response_content = open('generic_error.html','rb').read()
-                
-                response = b'HTTP/1.0 200 OK\r\nContent-Length: %d\r\n\r\n' % len(response_content)
+                    response_content = open('generic_error.html', 'rb').read()
+                    status = "400 Bad Request"
+
+                response = b'HTTP/1.0 {}\r\nContent-Length: {}\r\n\r\n'.format(status, len(response_content))
                 response += response_content
                 client.send(response)
                 client.close()
