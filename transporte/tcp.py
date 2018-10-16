a919465e411ba8e013080750f3b8ccd98cff3373
Cassiano Maia <cassmala@gmail.com>
2018-10-16 17:22:07 -0300

    Alteraçoes para o timer do ACK

--- transporte/tcp.py
+++ transporte/tcp.py
@@ -9,20 +9,37 @@
 import asyncio
 import socket
 import struct
+import os
+import random
 
 FLAGS_FIN = 1<<0
 FLAGS_SYN = 1<<1
 FLAGS_RST = 1<<2
 FLAGS_ACK = 1<<4
 
+MSS = 1460
+
+TESTAR_PERDA_ENVIO = False
+
+
+class Conexao:
+    def __init__(self, id_conexao, seq_no, ack_no, rtt=3):
+        self.id_conexao = id_conexao
+        self.seq_no = seq_no
+        self.ack_no = ack_no
+        self.send_queue = b"HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\n" + 1000000 * b"hello pombo\n"
+        self.rtt = rtt
+        self.nao_confirmado = []
+        self.timer = None
+conexoes = {}
+
+
 
 def addr2str(addr):
     return '%d.%d.%d.%d' % tuple(int(x) for x in addr)
 
-
 def str2addr(addr):
     return bytes(int(x) for x in addr.split('.'))
-
 
 def handle_ipv4_header(packet):
     version = packet[0] >> 4
@@ -34,13 +51,16 @@ def handle_ipv4_header(packet):
     return src_addr, dst_addr, segment
 
 
-def make_synack(src_port, dst_port, ack_no):
-    return struct.pack('!HHIIHHHH', src_port, dst_port, 0,
+def make_synack(src_port, dst_port, seq_no, ack_no):
+    return struct.pack('!HHIIHHHH', src_port, dst_port, seq_no,
                        ack_no, (5<<12)|FLAGS_ACK|FLAGS_SYN,
                        1024, 0, 0)
 
 
 def calc_checksum(segment):
+    if len(segment) % 2 == 1:
+        # se for ímpar, faz padding à direita
+        segment += b'\x00'
     checksum = 0
     for i in range(0, len(segment), 2):
         x, = struct.unpack('!H', segment[i:i+2])
@@ -49,7 +69,6 @@ def calc_checksum(segment):
             checksum = (checksum & 0xffff) + 1
     checksum = ~checksum
     return checksum & 0xffff
-
 
 def fix_checksum(segment, src_addr, dst_addr):
     pseudohdr = str2addr(src_addr) + str2addr(dst_addr) + \
@@ -60,9 +79,56 @@ def fix_checksum(segment, src_addr, dst_addr):
     return bytes(seg)
 
 
-def timer_test(src_addr, src_port):
-    print('5 segundos desde que aceitamos a conexão de %s:%d' %
-          (src_addr, src_port))
+def send_next(fd, conexao):
+    payload = conexao.send_queue[:MSS]
+    conexao.nao_confirmado.append(conexao.send_queue[:MSS])
+    conexao.send_queue = conexao.send_queue[MSS:]
+
+    (dst_addr, dst_port, src_addr, src_port) = conexao.id_conexao
+
+    segment = struct.pack('!HHIIHHHH', src_port, dst_port, conexao.seq_no,
+                          conexao.ack_no, (5<<12)|FLAGS_ACK,
+                          1024, 0, 0) + payload
+
+    conexao.seq_no = (conexao.seq_no + len(payload)) & 0xffffffff
+
+    segment = fix_checksum(segment, src_addr, dst_addr)
+
+    if not TESTAR_PERDA_ENVIO or random.random() < 0.95:
+        fd.sendto(segment, (dst_addr, dst_port))
+        conexao.timer = asyncio.get_event_loop().call_later(conexao.rtt, timeout, fd, conexao)
+
+    if conexao.send_queue == b"":
+        segment = struct.pack('!HHIIHHHH', src_port, dst_port, conexao.seq_no,
+                          conexao.ack_no, (5<<12)|FLAGS_FIN|FLAGS_ACK,
+                          0, 0, 0)
+        segment = fix_checksum(segment, src_addr, dst_addr)
+        fd.sendto(segment, (dst_addr, dst_port))
+
+
+def timeout(fd, conexao):
+	payload = conexao.naoconfirmado[:MSS]
+
+	(dst_addr, dst_port, src_addr, src_port) = conexao.id_conexao
+
+    segment = struct.pack('!HHIIHHHH', src_port, dst_port, conexao.seq_no,
+                          conexao.ack_no, (5<<12)|FLAGS_ACK,
+                          1024, 0, 0) + payload
+
+    conexao.seq_no = (conexao.seq_no + len(payload)) & 0xffffffff
+
+    segment = fix_checksum(segment, src_addr, dst_addr)
+
+    if not TESTAR_PERDA_ENVIO or random.random() < 0.95:
+        fd.sendto(segment, (dst_addr, dst_port))     
+        #conexao.timer = asyncio.get_event_loop().call_later(conexao.rtt, timeout, fd, conexao)
+
+    if conexao.send_queue == b"":
+        segment = struct.pack('!HHIIHHHH', src_port, dst_port, conexao.seq_no,
+                          conexao.ack_no, (5<<12)|FLAGS_FIN|FLAGS_ACK,
+                          0, 0, 0)
+        segment = fix_checksum(segment, src_addr, dst_addr)
+        fd.sendto(segment, (dst_addr, dst_port))
 
 
 def raw_recv(fd):
@@ -72,21 +138,48 @@ def raw_recv(fd):
         flags, window_size, checksum, urg_ptr = \
         struct.unpack('!HHIIHHHH', segment[:20])
 
+    id_conexao = (src_addr, src_port, dst_addr, dst_port)
+
     if dst_port != 7000:
         return
+
+    payload = segment[4*(flags>>12):]
 
     if (flags & FLAGS_SYN) == FLAGS_SYN:
         print('%s:%d -> %s:%d (seq=%d)' % (src_addr, src_port,
                                            dst_addr, dst_port, seq_no))
-        
-        fd.sendto(fix_checksum(make_synack(dst_port, src_port, seq_no + 1),
+
+        conexoes[id_conexao] = conexao = Conexao(id_conexao=id_conexao,
+                                                 seq_no=struct.unpack('I', os.urandom(4))[0],
+                                                 ack_no=seq_no + 1)
+
+        fd.sendto(fix_checksum(make_synack(dst_port, src_port, conexao.seq_no, conexao.ack_no),
                                src_addr, dst_addr),
                   (src_addr, src_port))
 
-        asyncio.get_event_loop().call_later(5, timer_test, src_addr, src_port)
+        conexao.seq_no += 1
+        asyncio.get_event_loop().call_later(.1, send_next, fd, conexao)
+    
+    elif id_conexao in conexoes:
+    	conexao = conexoes[id_conexao]
+    	if (flags & FLAGS_ACK) == FLAGS_ACK:
+    		received_ack(fd, conexao)
+    	#if(payload not zero)
+
+        conexao.ack_no += len(payload)
+
+    else:
+        print('%s:%d -> %s:%d (pacote associado a conexão desconhecida)' %
+            (src_addr, src_port, dst_addr, dst_port))
+
+def received_ack(fd, conexao):
+	if conexao.timer is not None:
+		conexao.timer.cancel()
+	
 
 
-fd = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-loop = asyncio.get_event_loop()
-loop.add_reader(fd, raw_recv, fd)
-loop.run_forever()
+if __name__ == '__main__':
+    fd = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+    loop = asyncio.get_event_loop()
+    loop.add_reader(fd, raw_recv, fd)
+    loop.run_forever()
